b4826eb195a8f024c48772030b6275aa82a827ba
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2026-07-15 01:21:17 +0530

    text formatting update

--- builtin/putf.go
+++ builtin/putf.go
@@ -3,6 +3,7 @@ package builtin
 import (
 	"fmt"
 	"mutant/object"
+	"strings"
 )
 
 func Putf(args ...object.Object) object.Object {
@@ -11,24 +12,29 @@ func Putf(args ...object.Object) object.Object {
 	}
 
 	format := args[0].Inspect()
+	if strings.Contains(format, "%") {
+		vals := make([]any, 0, len(args)-1)
+		for _, arg := range args[1:] {
+			switch v := arg.(type) {
+			case *object.Integer:
+				vals = append(vals, v.Value)
+			case *object.String:
+				vals = append(vals, v.Value)
+			case *object.Boolean:
+				vals = append(vals, v.Value)
+			case *object.Float:
+				vals = append(vals, v.Value)
+			default:
+				vals = append(vals, v.Inspect())
+			}
 
-	vals := make([]any, 0, len(args)-1)
-	for _, arg := range args[1:] {
-		switch v := arg.(type) {
-		case *object.Integer:
-			vals = append(vals, v.Value)
-		case *object.String:
-			vals = append(vals, v.Value)
-		case *object.Boolean:
-			vals = append(vals, v.Value)
-		case *object.Float:
-			vals = append(vals, v.Value)
-		default:
-			vals = append(vals, v.Inspect())
 		}
-
+		fmt.Printf(format, vals...)
+		return nil
 	}
 
-	fmt.Printf(format, vals...)
+	for _, arg := range args {
+		fmt.Printf("%v", arg.Inspect())
+	}
 	return nil
 }
